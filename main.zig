50bf4670802e9b7c7e9330262beb2416526971bf
nclanceman <snake560@gmail.com>
2023-07-23 17:44:31 -0500

    Add Modified Main

--- main.zig
+++ main.zig
@@ -1,5 +1,15 @@
 const std = @import("std");
+const utils = @import("utils.zig");
 
-pub fn main() void {
-    std.debug.print("Hellow, {s}!\n", .{"World"});
+pub fn main() !void {
+    std.debug.print("Hello, {s}!\n", .{"World"});
+
+    //Test: Roll 3d6
+    try utils.roller.init();
+    var result: u8 = 0;
+    var resultSet: [10]u8 = undefined;
+    result = utils.roller.roller(3, 6);
+    resultSet = utils.roller.retSet();
+    std.debug.print("The result of the test is : {}\n", .{result});
+    std.debug.print("The set of the dice reads: {any}\n", .{resultSet});
 }
